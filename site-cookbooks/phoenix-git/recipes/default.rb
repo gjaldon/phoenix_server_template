3a1fc2c2c6212ca17439126a0b4af2affc499cb0
Gabriel Jaldon <gjaldon85@gmail.com>
2015-07-11 19:50:21 +0800

    Install hex and rebar

--- site-cookbooks/phoenix-git/recipes/default.rb
+++ site-cookbooks/phoenix-git/recipes/default.rb
@@ -11,10 +11,20 @@ package "git"
 end
 
 execute "install hex" do
-  user "deploy"
-  cwd "/home/deploy"
-  command "HOME=/home/deploy mix local.hex --force"
-  creates "/home/deploy/.mix"
+  user "root"
+  group "deploy"
+  cwd "#{ENV["HOME"]}"
+  command "mix local.hex --force"
+  creates "#{ENV["HOME"]}/.mix/archives/hex.ez"
+  action :run
+end
+
+execute "install rebar" do
+  user "root"
+  group "deploy"
+  cwd "#{ENV["HOME"]}"
+  command "mix local.rebar"
+  creates "#{ENV["HOME"]}/.mix/rebar"
   action :run
 end
 
@@ -23,6 +33,7 @@ execute "set-up bare git repo" do
   creates "/var/repo/app.git/config"
   command "git init --bare"
   user "deploy"
+  group "deploy"
   action :run
 end
 
